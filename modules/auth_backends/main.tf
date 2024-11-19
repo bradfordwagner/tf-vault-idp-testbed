5614a3e8dc3947a0d6ebc0595f0fa97b8b183868
bradfordwagner <wagner.bradford@gmail.com>
2024-11-19 16:25:57 -0500

    vault auth

--- modules/auth_backends/main.tf
+++ modules/auth_backends/main.tf
@@ -45,3 +45,23 @@ resource "vault_jwt_auth_backend_role" "vault-oidc-role" {
   bound_claims         = { "roles" : each.value.idp_group }
   verbose_oidc_logging = true
 }
+
+# create kube auth backend
+resource "vault_auth_backend" "kubernetes_auth_endpoint" {
+  type      = "kubernetes"
+  path      = var.config.kubernetes.cluster.auth_endpoint
+  tune {
+    default_lease_ttl = var.config.kubernetes.cluster.default_lease_ttl
+  }
+}
+
+resource "vault_kubernetes_auth_backend_role" "smoke_test" {
+  for_each = var.config.kubernetes.roles
+  role_name                        = each.key
+  backend                          = vault_auth_backend.kubernetes_auth_endpoint.path
+  bound_service_account_names      = each.value.kubernetes.service_account_names
+  bound_service_account_namespaces = each.value.kubernetes.namespaces
+  token_policies = each.value.token_policies
+  alias_name_source = "serviceaccount_name"
+}
+
