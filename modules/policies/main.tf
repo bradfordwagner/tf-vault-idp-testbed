ff7586beefb252fbcd1db467beaac05fdf02eebc
bradfordwagner <wagner.bradford@gmail.com>
2024-11-19 16:30:36 -0500

    oidc client config

--- modules/policies/main.tf
+++ modules/policies/main.tf
@@ -6,3 +6,13 @@ path "*" {
 }
 EOT
 }
+
+# read any oidc client
+resource "vault_policy" "oidc_client" {
+  name   = "oidc_client"
+  policy = <<EOT
+path "identity/oidc/provider/*" {
+  capabilities = ["read"]
+}
+EOT
+}
