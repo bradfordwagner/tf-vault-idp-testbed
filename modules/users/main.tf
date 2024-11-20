312e3dd0f66a25b6017c9b716b081eeff6de1886
bradfordwagner <wagner.bradford@gmail.com>
2024-11-20 17:32:56 -0500

    cleanup

--- modules/users/main.tf
+++ modules/users/main.tf
@@ -18,28 +18,3 @@ resource "vault_identity_entity_alias" "user_pass" {
   canonical_id   = var.entity_name_to_id[each.key]
   mount_accessor = var.user_pass_accessor
 }
-
-# # set up identity groups
-# resource "vault_identity_group" "admins" {
-#   name     = "admins"
-#   type     = "internal"
-#   policies = ["default"]
-#   metadata = {
-#     version = "2"
-#   }
-#   member_entity_ids = [
-#     for id in [vault_identity_entity.user.id] : id if var.config.user.group == "admins"
-#   ]
-# }
-# resource "vault_identity_group" "users" {
-#   name     = "users"
-#   type     = "internal"
-#   policies = ["default"]
-#   metadata = {
-#     version = "2"
-#   }
-#   member_entity_ids = [
-#     for id in [vault_identity_entity.user.id] : id if var.config.user.group == "users"
-#   ]
-# }
-
